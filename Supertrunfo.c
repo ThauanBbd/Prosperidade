65fa73a03e8ed67e3be22a26e6c1f4afb743d9ee
ThauanBbd <thauan42furtado@gmail.com>
2025-10-21 13:18:05 -0300

    Initial implementation of Supertrunfo game
    Fix syntax errors and improve variable declarations.

---
+++ Supertrunfo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+main int ()
+{
+    char estado;
+    char código da carta;
+    \\ Inicial do estado e número de 01 a 04.
+    char nome da cidade;
+    int população;
+    float área em km2;
+    float pib;
+    int número de pontos turísticos;
+
+            printf("Digite o nome do estado: \n");
+            scanf("%s", %estado);
+
+            printf("Diite o código da carta: \n");
+            scanf("%s", Código da carta);
+
+            printf("Digite o nome da ciddade: \n");
+            scanf("%s", &nome da cidade);
+
+            pintf("Digite a população: \n");
+            scanf("%d", &populção);
+
+            printf("Digite a área em km2: \n");
+            scanf("%e", &área em km2);
+
+            printf("Digite o PIB: \n");
+            scanf("%e", &pib);
+
+            printf("Digite o número de pontos turísticos: \n");
+            scanf("%d", &número de pontos turísticos);
+
+    
+    return 0;
+}
